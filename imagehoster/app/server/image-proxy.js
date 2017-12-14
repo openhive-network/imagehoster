d9f613f92da2de79ad2324fadb3d2d11cd60840b
Gandalf <Gandalf-the-Grey@users.noreply.github.com>
2017-12-14 20:33:07 +0100

    updated image blacklist

--- imagehoster/app/server/image-proxy.js
+++ imagehoster/app/server/image-proxy.js
@@ -63,6 +63,7 @@ router.get('/:width(\\d+)x:height(\\d+)/:url(.*)', function *() {
         'http://edge.liveleak.com/80281E/ll_a_u/thumbs/2015/Jan/1/67f252081582_sf_3.jpg',
         'http://st-listas.20minutos.es/images/2016-03/408680/list_640px.jpg?1458217580',
         'http://i1272.photobucket.com/albums/y391/mtgmtg_2012/mtgmtg_2012006/8575314572_bb657293cd_b_zps4d684b87.jpg',
+        'http://img09.deviantart.net/c561/i/2015/005/4/b/psychedeliczen_id_by_psychedeliczen-d63npyv.jpg',
     ];
     if(blacklist.includes(url)) {
         statusError(this, 400, 'Bad Request')
