94acb53d1c57e67e59f48c210ba2719f3f46ab17
Justin Welch <juswelch@gmail.com>
2019-09-17 13:37:25 -0400

    dmca-20190917

--- src/blacklist.ts
+++ src/blacklist.ts
@@ -124,7 +124,8 @@ export const imageBlacklist: Blacklist<string> = [
     'https://68.media.tumblr.com/d71364f2c1adac1ced066a25193c9fee/tumblr_ob8ccl51EN1rs7mnao4_500.jpg',
     'https://68.media.tumblr.com/70352eab4db092ec434895ce02afbba4/tumblr_ob8ccl51EN1rs7mnao3_500.jpg',
     'http://www.aquariumfish.net/images_01/congo_tetra_121104a2_w0640.jpg',
-    'http://i.imgsafe.org/82229b8f14.jpg'
+    'http://i.imgsafe.org/82229b8f14.jpg',
+    'http://twistedsifter.files.wordpress.com/2012/06/the-honeybees-final-sting.jpg'
   ]
 
 export const accountBlacklist: Blacklist<string> = [
