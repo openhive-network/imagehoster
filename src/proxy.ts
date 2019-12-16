ff34821e1247b4fbd15979f0d7597f3f4f64a69a
Quoc Huy Nguyen Dinh <quochuy@gmail.com>
2019-12-16 20:26:49 +1100

    Automatically rotate images that were taken in portrait mode froma mobile phone

--- src/proxy.ts
+++ src/proxy.ts
@@ -319,13 +319,13 @@ export async function proxyHandler(ctx: KoaContext) {
 
         switch (options.mode) {
             case ScalingMode.Cover:
-                image.resize(width, height).crop()
+                image.rotate().resize(width, height).crop()
                 break
             case ScalingMode.Fit:
                 if (!width) { width = maxWidth }
                 if (!height) { height = maxHeight }
 
-                image.resize(width, height).max().withoutEnlargement()
+                image.rotate().resize(width, height).max().withoutEnlargement()
                 break
         }
 
