a5a34fd654c983f07ae279663d8f0213ad8dcb56
Rishi Panthee <rrpanthe@ncsu.edu>
2023-09-16 22:20:16 -0500

    Convert heic to jpeg
    This currently fails using the libvips that ships with sharp, need to install globalvips for it to work, have not yet tested with globalvips

--- src/serve.ts
+++ src/serve.ts
@@ -1,7 +1,6 @@
 /** Serve files from upload store. */
-
+import * as Sharp from 'sharp'
 import streamHead from 'stream-head/dist-es6'
-
 import {KoaContext, uploadStore} from './common'
 import {APIError} from './error'
 import {mimeMagic} from './utils'
@@ -25,9 +24,14 @@ export async function serveHandler(ctx: KoaContext) {
     })
 
     const {head, stream} = await streamHead(file, {bytes: 16384})
-    const mimeType = await mimeMagic(head)
-
+    const bodyStream: any = stream
+    let mimeType = await mimeMagic(head)
+    if (mimeType === 'image/heic') {
+        const toJPEG = Sharp().toFormat('jpeg').jpeg({quality: 80})
+        mimeType = 'image/jpeg'
+        stream.pipe(toJPEG).pipe(bodyStream)
+    }
     ctx.response.set('Content-Type', mimeType)
     ctx.response.set('Cache-Control', 'public,max-age=29030400,immutable')
-    ctx.body = stream
+    ctx.body = bodyStream
 }
