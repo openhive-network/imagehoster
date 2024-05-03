62e6ba4b2f70e3ab50112b15d13cef771638109b
Eric Frias <efrias@syncad.com>
2024-05-03 18:53:09 +0000

    New plan, cache avatar redirects for a day

--- src/avatar.ts
+++ src/avatar.ts
@@ -73,10 +73,19 @@ export async function avatarHandler(ctx: KoaContext) {
         avatarUrl = metadata.profile.profile_image
     }
 
+    // this was the original setting (10m), likely intended to be a balance between allowing some caching
+    // and having your avatar change fairly quickly after you update your account profile.
+    // ctx.set('Cache-Control', 'public,max-age=600')
+    //
+    // We found this to be way too short, so the next attempt was (forever):
     // avatars aren't immutable, of course, but we're marking them as immutable to have them
     // cached forever by varnish/cloudflare.  We run a helper application that monitors accounts
     // for updates, and purges the cache for those accounts whenever that happens.
-    ctx.set('Cache-Control', 'public,max-age=29030400,immutable')
+    // this worked well as far as varnish & cloudflare were concerned, because we proactively tell
+    // them to invalidate.  But the users's browser cache will also use this age, and we have no way
+    // of telling the user's browser to invalidate their cache.  So now we set it to 1 day as a bit
+    // of a compromise.
+    ctx.set('Cache-Control', 'public,max-age=86400')
 
     ctx.set('Server-Timing', `api;dur=${(timeAfterGetAccounts - timeBeforeGetAccounts).toFixed(3)};desc="get_accounts API call"`)
     ctx.redirect(`/p/${ base58Enc(avatarUrl) }?width=${ size }&height=${ size }`)
